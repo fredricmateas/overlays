91dde508a2973213a43147b7b46d57ee54c88b2d
fredricmateas <133617230+fredricmateas@users.noreply.github.com>
2023-05-15 09:38:33 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+
+int main(){
+
+Printf ("transfer to exelp function \n"); 
+execll ("./oup","jour", NULL) Prints (this line will not execute(""); return 0;
+}
+int main() {
+
+Printf ("transfer to execlp function \n") 
+char * arr[] = { head", "head", "-")", "fi", NULL)}; 
+execl("./oup", arr);
+
+Print ("this line will not execution")
+
+Return 0;
+
+
+
+int main() {
+
+        Printf("transfer to exelp function \n");
+        execll("./oup", "jour", NULL)
+        Prints(this line will not execute("");
+            return 0;
+        }
+
+
+int main() {
+
+Printf ("transfer to execlp function \n") 
+char * arr[] = { head", "head", "-")", "fi", NULL)}; 
+execl("./oup", arr);
+
+Print ("this line will not execution")
+
+Return 0;
